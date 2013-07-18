4a9ce85840ea225876134f95a7b29eae5fa5d6e3
ErgoZ <ergozru@gmail.com>
2013-07-19 01:30:05 +0400

    update votes.js

--- votes.js
+++ votes.js
@@ -1,1 +1,498 @@
-window.lk.votes(42083)
+<!doctype html>
+
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="ru"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="ru"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="ru"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="ru"> <!--<![endif]-->
+
+<head>
+	
+	
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title>Altroism</title>
+	
+	<meta name="description" content="Пишем грамотные петиции вместе">
+	<meta name="keywords" content="ROI, Российская Общественная Инициатива, петиции, законы, Россия">
+
+	<link rel='stylesheet' type='text/css' href='http://showalert.org/roi/templates/cache/synio/b04e899741c9c50390d71391bee054b0.css' />
+
+	
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+
+	<link href="http://showalert.org/roi/templates/skin/synio/images/favicon.ico?v1" rel="shortcut icon" />
+	<link rel="search" type="application/opensearchdescription+xml" href="http://showalert.org/roi/search/opensearch/" title="Altroism" />
+
+			<link rel="alternate" type="application/rss+xml" href="http://showalert.org/roi/rss/index/" title="Altroism">
+	
+			<link rel="canonical" href="http://showalert.org/roi/" />
+	
+		
+	
+	<script type="text/javascript">
+		var DIR_WEB_ROOT 			= 'http://showalert.org/roi';
+		var DIR_STATIC_SKIN 		= 'http://showalert.org/roi/templates/skin/synio';
+		var DIR_ROOT_ENGINE_LIB 	= 'http://showalert.org/roi/engine/lib';
+		var LIVESTREET_SECURITY_KEY = '1089f74381963a9cc98c4cada572697e';
+		var SESSION_ID				= 'n4h4buaeffku21ru9ecpkhm5u1';
+		var BLOG_USE_TINYMCE		= '';
+		
+		var TINYMCE_LANG = 'en';
+					TINYMCE_LANG = 'ru';
+		
+		var aRouter = new Array();
+					aRouter['error'] = 'http://showalert.org/roi/error/';
+					aRouter['registration'] = 'http://showalert.org/roi/registration/';
+					aRouter['profile'] = 'http://showalert.org/roi/profile/';
+					aRouter['my'] = 'http://showalert.org/roi/my/';
+					aRouter['blog'] = 'http://showalert.org/roi/blog/';
+					aRouter['personal_blog'] = 'http://showalert.org/roi/personal_blog/';
+					aRouter['index'] = 'http://showalert.org/roi/index/';
+					aRouter['topic'] = 'http://showalert.org/roi/topic/';
+					aRouter['login'] = 'http://showalert.org/roi/login/';
+					aRouter['people'] = 'http://showalert.org/roi/people/';
+					aRouter['settings'] = 'http://showalert.org/roi/settings/';
+					aRouter['tag'] = 'http://showalert.org/roi/tag/';
+					aRouter['talk'] = 'http://showalert.org/roi/talk/';
+					aRouter['comments'] = 'http://showalert.org/roi/comments/';
+					aRouter['rss'] = 'http://showalert.org/roi/rss/';
+					aRouter['link'] = 'http://showalert.org/roi/link/';
+					aRouter['question'] = 'http://showalert.org/roi/question/';
+					aRouter['blogs'] = 'http://showalert.org/roi/blogs/';
+					aRouter['search'] = 'http://showalert.org/roi/search/';
+					aRouter['admin'] = 'http://showalert.org/roi/admin/';
+					aRouter['ajax'] = 'http://showalert.org/roi/ajax/';
+					aRouter['feed'] = 'http://showalert.org/roi/feed/';
+					aRouter['stream'] = 'http://showalert.org/roi/stream/';
+					aRouter['photoset'] = 'http://showalert.org/roi/photoset/';
+					aRouter['subscribe'] = 'http://showalert.org/roi/subscribe/';
+			</script>
+	
+	
+	<script type='text/javascript' src='http://showalert.org/roi/templates/cache/synio/9236f366164c1a6d75926a334523b54d.js'></script>
+<!--[if lt IE 9]><script type='text/javascript' src='http://showalert.org/roi/engine/lib/external/html5shiv.js'></script><![endif]-->
+<script type='text/javascript' src='http://yandex.st/share/share.js'></script>
+
+
+	
+	<script type="text/javascript">
+		var tinyMCE = false;
+		ls.lang.load([]);
+		ls.registry.set('comment_max_tree',5);
+		ls.registry.set('block_stream_show_tip',true);
+	</script>
+	
+	
+			<style>
+			#container {
+				width: 976px;
+			}
+		</style>
+		
+	
+	
+</head>
+
+
+
+	
+	
+
+
+
+<body class=" ls-user-role-guest ls-user-role-not-admin width-fixed">
+	
+	
+	
+				<div class="modal modal-login" id="window_login_form">
+		<header class="modal-header">
+			<a href="#" class="close jqmClose"></a>
+		</header>
+		
+		
+		<script type="text/javascript">
+			jQuery(function($){
+				$('#popup-login-form').bind('submit',function(){
+					ls.user.login('popup-login-form');
+					return false;
+				});
+				$('#popup-login-form-submit').attr('disabled',false);
+			});
+		</script>
+		
+		<div class="modal-content">
+			<ul class="nav nav-pills nav-pills-tabs">
+				<li class="active js-block-popup-login-item" data-type="login"><a href="#">Войти</a></li>
+									<li class="js-block-popup-login-item" data-type="registration"><a href="#">Регистрация</a></li>
+								<li class="js-block-popup-login-item" data-type="reminder"><a href="#">Восстановление пароля</a></li>
+			</ul>
+			
+			
+			<div class="tab-content js-block-popup-login-content" data-type="login">
+				
+				<form action="http://showalert.org/roi/login/" method="post" id="popup-login-form">
+					
+
+					<p><input type="text" name="login" id="popup-login" placeholder="Логин или эл. почта" class="input-text input-width-full"></p>
+					
+					<p><input type="password" name="password" id="popup-password" placeholder="Пароль" class="input-text input-width-300" style="width: 322px">
+					<button type="submit"  name="submit_login" class="button button-primary" id="popup-login-form-submit" disabled="disabled">Войти</button></p>
+					
+					<label class="remember-label"><input type="checkbox" name="remember" class="input-checkbox" checked /> Запомнить меня</label>
+
+					<small class="validate-error-hide validate-error-login"></small>
+					
+
+					<input type="hidden" name="return-path" value="http://showalert.org/roi/">
+				</form>
+				
+			</div>
+
+
+						<div data-type="registration" class="tab-content js-block-popup-login-content" style="display:none;">
+				<script type="text/javascript">
+					jQuery(document).ready(function($){
+						$('#popup-registration-form').find('input.js-ajax-validate').blur(function(e){
+							var aParams={ };
+							if ($(e.target).attr('name')=='password_confirm') {
+								aParams['password']=$('#popup-registration-user-password').val();
+							}
+							if ($(e.target).attr('name')=='password') {
+								aParams['password']=$('#popup-registration-user-password').val();
+								if ($('#popup-registration-user-password-confirm').val()) {
+									ls.user.validateRegistrationField('password_confirm',$('#popup-registration-user-password-confirm').val(),$('#popup-registration-form'),{ 'password': $(e.target).val() });
+								}
+							}
+							ls.user.validateRegistrationField($(e.target).attr('name'),$(e.target).val(),$('#popup-registration-form'),aParams);
+						});
+						$('#popup-registration-form').bind('submit',function(){
+							ls.user.registration('popup-registration-form');
+							return false;
+						});
+						$('#popup-registration-form-submit').attr('disabled',false);
+					});
+				</script>
+
+				
+				<form action="http://showalert.org/roi/registration/" method="post" id="popup-registration-form">
+					
+
+					<p>
+					<input type="text" name="login" placeholder="Логин" id="popup-registration-login" value="" class="input-text input-width-300 js-ajax-validate" />
+					<i class="icon-question-sign js-tip-help" title="Может состоять только из букв (A-Z a-z), цифр (0-9). Знак подчеркивания (_) лучше не использовать. Длина логина не может быть меньше 3 и больше 30 символов."></i>
+					<i class="icon-ok-green validate-ok-field-login" style="display: none"></i>
+					<small class="validate-error-hide validate-error-field-login"></small></p>
+
+					<p>
+					<input type="text" name="mail" placeholder="E-mail" id="popup-registration-mail" value="" class="input-text input-width-300 js-ajax-validate" />
+					<i class="icon-question-sign js-tip-help" title="Для проверки регистрации и в целях безопасности нам нужен адрес вашей электропочты."></i>
+					<i class="icon-ok-green validate-ok-field-mail" style="display: none"></i>
+					<small class="validate-error-hide validate-error-field-mail"></small></p>
+
+					<p>
+					<input type="password" name="password" placeholder="Пароль" id="popup-registration-user-password" value="" class="input-text input-width-300 js-ajax-validate" />
+					<i class="icon-question-sign js-tip-help" title="Должен содержать не менее 5 символов и не может совпадать с логином. Не используйте простые пароли, будьте разумны."></i>
+					<i class="icon-ok-green validate-ok-field-password" style="display: none"></i>
+					<small class="validate-error-hide validate-error-field-password"></small></p>
+
+					<p>
+					<input type="password" name="password_confirm" placeholder="Повторите пароль" id="popup-registration-user-password-confirm" value="" class="input-text input-width-300 js-ajax-validate" />
+					<i class="icon-ok-green validate-ok-field-password_confirm" style="display: none"></i>
+					<small class="validate-error-hide validate-error-field-password_confirm"></small></p>
+
+					
+					<p><label for="popup-registration-captcha">Введите цифры и буквы</label>
+					<img src="http://showalert.org/roi/engine/lib/external/kcaptcha/index.php?PHPSESSID=n4h4buaeffku21ru9ecpkhm5u1" 
+						 onclick="this.src='http://showalert.org/roi/engine/lib/external/kcaptcha/index.php?PHPSESSID=n4h4buaeffku21ru9ecpkhm5u1&n='+Math.random();"
+						 class="captcha-image" />
+					<input type="text" name="captcha" id="popup-registration-captcha" value="" maxlength="3" class="input-text input-width-100 js-ajax-validate" />
+					<small class="validate-error-hide validate-error-field-captcha"></small></p>
+					
+
+					
+
+					<input type="hidden" name="return-path" value="http://showalert.org/roi/">
+					<button type="submit"  name="submit_register" class="button button-primary" id="popup-registration-form-submit" disabled="disabled">Зарегистрироваться</button>
+				</form>
+				
+			</div>
+						
+			
+			<div data-type="reminder" class="tab-content js-block-popup-login-content" style="display:none;">
+				<script type="text/javascript">
+					jQuery(document).ready(function($){
+						$('#popup-reminder-form').bind('submit',function(){
+							ls.user.reminder('popup-reminder-form');
+							return false;
+						});
+						$('#popup-reminder-form-submit').attr('disabled',false);
+					});
+				</script>
+				<form action="http://showalert.org/roi/login/reminder/" method="POST" id="popup-reminder-form">
+					<p>
+					<input type="text" name="mail" placeholder="Ваш e-mail" id="popup-reminder-mail" class="input-text input-width-300" />
+					<small class="validate-error-hide validate-error-reminder"></small></p>
+
+					<button type="submit" name="submit_reminder" class="button button-primary" id="popup-reminder-form-submit" disabled="disabled">Получить ссылку на изменение пароля</button>
+				</form>
+			</div>
+		</div>
+	</div>
+
+		
+
+
+	
+	<div id="header-back"></div>
+	
+	<div id="container" class="">
+		<header id="header" role="banner">
+	
+	<h1 class="site-name"><a href="http://showalert.org/roi">Altroism</a></h1>
+	
+	
+	<ul class="nav nav-main" id="nav-main">
+		<li class="active"><a href="http://showalert.org/roi">Топики</a> <i></i></li>
+		<li ><a href="http://showalert.org/roi/blogs/">Блоги</a> <i></i></li>
+		<li ><a href="http://showalert.org/roi/people/">Люди</a> <i></i></li>
+		<li ><a href="http://showalert.org/roi/stream/">Активность</a> <i></i></li>
+
+		
+
+		<li class="nav-main-more"><a href="#" id="dropdown-mainmenu-trigger" onclick="return false">еще</a></li>
+	</ul>
+
+	<ul class="dropdown-nav-main dropdown-menu" id="dropdown-mainmenu-menu"></ul>
+
+	
+	
+	
+	
+	
+			<ul class="auth">
+			
+			<li><a href="http://showalert.org/roi/registration/" class="js-registration-form-show">Зарегистрироваться</a></li>
+			<li><a href="http://showalert.org/roi/login/" class="js-login-form-show sign-in">Войти</a></li>
+		</ul>
+		
+		
+	
+	
+</header>
+
+		<nav id="nav">
+			<ul class="nav nav-menu">
+	<li class="active">
+		<a href="http://showalert.org/roi/">Все</a>
+	</li>
+
+	<li >
+		<a href="http://showalert.org/roi/blog/">Коллективные</a>
+	</li>
+
+	<li >
+		<a href="http://showalert.org/roi/personal_blog/">Персональные</a>
+	</li>
+	
+	
+	
+</ul>		
+		
+	<div class="search-header">
+		<div class="search-header-show" id="search-header-show"><i class="icon-synio-search"></i> <a href="#" class="link-dotted">Найти</a></div>
+		
+		<form class="search-header-form" id="search-header-form" action="http://showalert.org/roi/search/topics/" style="display: none">
+			<input type="text" placeholder="Поиск" maxlength="255" name="q" class="input-text">
+			<input type="submit" value="" title="Найти" class="input-submit">
+		</form>
+	</div>
+</nav>
+
+		<div id="wrapper" class="">
+							<aside id="sidebar" >
+	
+
+						<section class="block block-type-stream">
+	
+
+	<header class="block-header sep">
+		<h3><a href="http://showalert.org/roi/comments/" title="Весь эфир">Прямой эфир</a></h3>
+		<div class="block-update js-block-stream-update"></div>
+		
+		<ul class="nav nav-pills js-block-stream-nav" >
+			<li class="active js-block-stream-item" data-type="comment"><a href="#">Комментарии</a></li>
+			<li class="js-block-stream-item" data-type="topic"><a href="#">Публикации</a></li>
+			
+		</ul>
+		
+		<ul class="nav nav-pills js-block-stream-dropdown" style="display: none;">
+			<li class="dropdown active js-block-stream-dropdown-trigger"><a href="#">Комментарии</a> <i class="icon-synio-arrows"></i>
+				<ul class="dropdown-menu js-block-stream-dropdown-items">
+					<li class="active js-block-stream-item" data-type="comment"><a href="#">Комментарии</a></li>
+					<li class="js-block-stream-item" data-type="topic"><a href="#">Публикации</a></li>
+					
+				</ul>
+			</li>
+		</ul>
+	</header>
+	
+	<div class="block-content">
+		<div class="js-block-stream-content">
+			
+		</div>
+	</div>
+</section>
+
+
+										<section class="block block-type-tags">
+	<header class="block-header sep">
+		<h3>Теги</h3>
+		
+		<ul class="nav nav-pills">
+			<li class="active js-block-tags-item" data-type="all"><a href="#">Все теги</a></li>
+			
+			
+		</ul>
+	</header>
+	
+	
+	<div class="block-content">
+		<form action="" method="GET" class="js-tag-search-form search-form-wrapper">
+			<div class="search-input-wrapper">
+				<input type="text" name="tag" placeholder="Поиск тегов" value="" class="input-text input-width-full autocomplete-tags js-tag-search" />
+				<div class="input-submit" onclick="jQuery('.js-tag-search-form').submit()"></div>
+			</div>
+		</form>
+
+		<div class="js-block-tags-content" data-type="all">
+							<div class="notice-empty">Нет тегов</div>
+					</div>
+
+			</div>
+</section>
+										<div class="block block-type-blogs" id="block_blogs">
+	<header class="block-header sep">
+		<h3>Блоги</h3>
+		<div class="block-update js-block-blogs-update"></div>
+		
+		<ul class="nav nav-pills js-block-blogs-nav">
+			<li class="active js-block-blogs-item" data-type="top"><a href="#">Топ</a></li>
+					</ul>
+	</header>
+	
+	
+	<div class="block-content">
+		
+		
+		<div class="js-block-blogs-content">
+			<ul class="block-blog-list">
+	</ul>				
+		</div>
+
+		
+		<footer>
+			<a href="http://showalert.org/roi/blogs/">Все блоги</a>
+		</footer>
+	</div>
+</div>
+
+					
+</aside>
+					
+			<div id="content" role="main"  >
+						<ul class="nav nav-pills mb-30">
+		<li class="active"><a href="http://showalert.org/roi/">Интересные</a></li>
+		<li >
+			<a href="http://showalert.org/roi/index/newall/" title="За все время">Новые</a>
+					</li>
+		<li ><a href="http://showalert.org/roi/index/discussed/">Обсуждаемые</a></li>
+		<li ><a href="http://showalert.org/roi/index/top/">TOP</a></li>
+		
+	</ul>
+
+
+
+
+
+
+					
+
+	
+				
+				
+	Сюда еще никто не успел написать
+
+			
+		</div> <!-- /content -->
+	</div> <!-- /wrapper -->
+
+
+	
+	<footer id="footer">
+					<ul class="footer-list">
+				<li class="footer-list-header word-wrap">Вы можете</li>
+				<li><a href="http://showalert.org/roi/registration/" class="js-registration-form-show">Зарегистрироваться</a></li>
+				<li><a href="http://showalert.org/roi/login/" class="js-login-form-show sign-in">Войти</a></li>
+				
+			</ul>
+				
+		<ul class="footer-list">
+			<li class="footer-list-header">Разделы</li>
+			<li><a href="http://showalert.org/roi">Топики</a></li>
+			<li><a href="http://showalert.org/roi/blogs/">Блоги</a></li>
+			<li><a href="http://showalert.org/roi/people/">Люди</a></li>
+			<li><a href="http://showalert.org/roi/stream/">Активность</a></li>
+			
+		</ul>
+		
+		
+		
+		
+		
+		
+	
+		<div class="copyright">
+			&copy; Powered by <a href="http://livestreet.ru/?utm_source=site&utm_medium=copyright&utm_campaign=livestreet">LiveStreet CMS</a>
+			
+			<div class="design-by">
+				<img src="http://showalert.org/roi/templates/skin/synio/images/xeoart.png" alt="xeoart" />
+				Design by <a href="http://xeoart.com">xeoart</a>
+				<div>2012</div>
+			</div>
+		</div>
+		
+		
+	</footer>
+</div> <!-- /container -->
+
+<aside class="toolbar" id="toolbar">
+	
+
+								
+										<section class="toolbar-scrollup" id="toolbar_scrollup">
+	<a href="#" onclick="return ls.toolbar.up.goUp();" title="Вверх"><i></i></a>
+</section>
+			
+</aside>
+
+
+			<script type="text/javascript">
+			var _lsIsLoadGA=(typeof(window._gaq)=='undefined') ? false : true ;
+
+			  var _gaq = _gaq || [];
+			  _gaq.push(['lscounter._setAccount', 'UA-28922093-1']);
+			  _gaq.push(['lscounter._trackPageview']);
+
+			if (!_lsIsLoadGA) {
+			  (function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();
+			}
+			</script>
+		
+
+</body>
+</html>
